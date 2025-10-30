196e8c9526220858e89afa214f544e1a19e9c694
Premjeet Vivek <premjeetvivek94@gmail.com>
2025-10-30 18:15:51 +0530

    Exploring get,put,delete api calls

--- src/app.js
+++ src/app.js
@@ -2,9 +2,24 @@ const express = require(`express`);
 
 const app = express();
 
+app.get("/user", (req,res) => {
+    res.send({Firstname: "Premjeet", Lastname: "Vivek"});
+});
+
+app.post("/user", (req,res) => {
+    res.send("Data successfully saved to the Db");
+});
+
+app.delete("/user", (req,res) => {
+    res.send("Deleted successfully");
+});
+
+
 app.use("/test",(req,res) => {
     res.send("hello from server")
 });
+
+
 
 app.listen(3000, () => {
     console.log("server is successfully listening on port 3000...");
